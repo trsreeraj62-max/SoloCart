cc5d74f6278b6222f0078623a260690c4c876ac1
sreerajtr <trsreeraj07@gmail.com>
2026-01-22 21:07:39 +0530

    Fix Discount Schema: Add discount columns to products table

---
+++ database/migrations/2026_01_22_153452_add_discount_columns_to_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            if (!Schema::hasColumn('products', 'discount_percent')) {
+                $table->integer('discount_percent')->default(0);
+                $table->timestamp('discount_start_date')->nullable();
+                $table->timestamp('discount_end_date')->nullable();
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
