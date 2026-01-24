1ee3d66d820ee20fc4644886f5f0840202c52a8d
sreerajtr <trsreeraj07@gmail.com>
2026-01-24 14:57:22 +0530

    Add Cloudinary support as alternative to S3 for profile images

--- config/filesystems.php
+++ config/filesystems.php
@@ -62,6 +62,16 @@ return [
             'report' => false,
         ],
 
+        'cloudinary' => [
+            'driver' => 'cloudinary',
+            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+            'api_key' => env('CLOUDINARY_API_KEY'),
+            'api_secret' => env('CLOUDINARY_API_SECRET'),
+            'url' => [
+                'secure' => true,
+            ],
+        ],
+
     ],
 
     /*
